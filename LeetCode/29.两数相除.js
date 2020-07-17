28bb206c7144a5065b959a9aff7fe37fe0693a13
meitaiqiao@100tal.com <meitaiqiao@100tal.com>
2020-07-17 14:12:14 +0800

    丢人的边界判断

--- LeetCode/29.两数相除.js
+++ LeetCode/29.两数相除.js
@@ -53,6 +53,18 @@
  * @return {number}
  */
 var divide = function(dividend, divisor) {
+    if (dividend >= 2147483647 && divisor === 1) {
+        return 2147483647;
+    }
+    if (dividend >= 2147483647 && divisor === -1) {
+        return -2147483648;
+    }
+    if (dividend <= -2147483648 && divisor === 1) {
+        return -2147483648;
+    }
+    if (dividend <= -2147483648 && divisor === -1) {
+        return 2147483647;
+    }
     var result = 0;
     var newDividend = Math.abs(dividend)
     var newDivisor = Math.abs(divisor)
